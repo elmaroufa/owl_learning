370793a3258fd6ae906b5d7d993b317721f5aa45
abbo-servoo <salyabbo@gmail.com>
2023-11-22 16:56:48 +0100

    end feature edit,delete task

--- app.js
+++ app.js
@@ -2,29 +2,54 @@ const {Component, mount, xml, useState} = owl
 
 class Task extends Component{
   static template = xml `
-  <li  t-attf-style="background-color:#{props.task.color}" class="d-flex align-items-center 
-  justify-content-between 
-  border p-3 mb-2 rounded">
-  <div class="form-check form-switch fs-5">
-      <input class="form-check-input" type="checkbox" value="" t-att-id="props.task.id" 
-      t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
-      <label class="form-check-label"  t-att-for="props.task.id"
-      t-attf-class="#{props.task.isCompleted ? 'text-decoration-line-through' : ''}">
-        <t t-esc="props.task.name" />
+  <li  t-attf-style="background-color:#{state.color}" class="d-flex align-items-center 
+  justify-content-between border p-3 mb-2 rounded">
+  <div t-if="state.isEditing" class="d-flex align-items-center flex-grow-1 me-2">
+    <input type="text" class="form-control m-2"  t-model="state.name"/>
+    <input type="color" style="width:60px" class="form-control-lg form-control-color border-0 bg-white" id="color" 
+       title="Choose your color" t-model="state.color" />
+  </div>
+  <div t-if="!state.isEditing" class="form-check form-switch fs-5">
+      <input class="form-check-input" type="checkbox" value="" t-att-id="state.id" 
+      t-att-checked="state.isCompleted" t-on-click="toggleTask" t-model="state.isCompleted"/>
+      <label class="form-check-label"  t-att-for="state.id"
+      t-attf-class="#{state.isCompleted ? 'text-decoration-line-through' : ''}">
+        <t t-esc="state.name" />
       </label>
     </div>
     <div>
-      <button class="btn btn-primary m-2"><i class="bi bi-pencil"></i></button>
-      <button class="btn btn-danger" t-on-click="deleteTask" ><i class="bi bi-trash"></i></button>
+      <button t-if="!state.isEditing" class="btn btn-primary m-2" t-on-click="editTask"><i class="bi bi-pencil"></i></button>
+      <button t-if="state.isEditing" class="btn btn-primary m-2" t-on-click="saveTask"><i class="bi bi-check"></i></button>
+      <button  class="btn btn-danger" t-on-click="deleteTask" ><i class="bi bi-trash"></i></button>
     </div>
   </li>
   `
-  static props = ["task", "onDelete"]
+  static props = ["task", "onDelete", "onEdit"]
+
+  setup(){
+    this.state  = useState({
+      isEditing: false,
+      id: this.props.task.id,
+      name: this.props.task.name,
+      color: this.props.task.color,
+      isCompleted : this.props.task.isCompleted
+    })
+  }
+
   toggleTask(){
-    this.props.task.isCompleted = !this.props.task.isCompleted
+    this.state.isCompleted = !this.state.isCompleted
   }
   deleteTask(){
     this.props.onDelete(this.props.task)
+  }
+
+  editTask(){
+    this.state.isEditing = true
+  }
+
+  saveTask(){
+    this.state.isEditing = false
+    this.props.onEdit(this.state)
   }
 }
 class Root extends Component{
@@ -42,7 +67,7 @@ class Root extends Component{
             </div>
             <ul class="d-flex flex-column mt-5 p-0">
               <t t-foreach="tasks" t-as="task" t-key="task.id">
-                <Task task="task"  onDelete.bind="deleteTask"/>
+                <Task task="task"  onDelete.bind="deleteTask" onEdit.bind="editTask"/>
               </t>
             </ul>
     `
@@ -77,6 +102,11 @@ class Root extends Component{
     deleteTask(task){
       const index = this.tasks.findIndex(t=> t.id == task.id)
       this.tasks.splice(index, 1)
+    }
+
+    editTask(task){
+      const index = this.tasks.findIndex(t=> t.id == task.id)
+      this.tasks.splice(index, 1, task)
     }
 }
 
